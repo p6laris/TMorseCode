df815addb410b6c6afcc667569cc8225ec1095dd
P0lariss <Yunissurchy@gmail.com>
2021-06-20 22:34:04 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Media;
+
+
+namespace TMorseCode
+{
+    public partial class MainWindow : Window
+    {
+        SolidColorBrush black = new SolidColorBrush(Color.FromRgb(0,0,0));
+        SolidColorBrush white = new SolidColorBrush(Color.FromRgb(100, 100, 100));
+        SoundPlayer dotPlayer = new SoundPlayer("dot.wav");
+        SoundPlayer dashPlayer = new SoundPlayer("dash.wav");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        private void exitClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void aboutClick(object sender, RoutedEventArgs e)
+        {
+            About p = new About();
+            p.ShowDialog();
+        }
+
+        private void morseTxt_GotFocus(object sender, RoutedEventArgs e)
+        {
+            morseTxt.Clear();
+
+        }
+
+        private void convertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            txtBlock.Text = "";
+            List<char> newArray = new List<char>();
+            
+            if(string.IsNullOrEmpty(morseTxt.Text) || morseTxt.Text == "Type anything....")
+            {
+                MessageBox.Show("Please type something to convert.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                playBtn.IsEnabled = true;
+                copyBtn.IsEnabled = true;
+                char[][] morse = MorseConverter.MorseCode();
+                for (int i = 0; i < morseTxt.Text.Length; i++)
+                {
+                    bool isLetterFound = false;
+                    if(morseTxt.Text[i] == ' ')
+                    {
+                        ShowMorse(' ');
+                    }
+                    else
+                    {
+                        for (int j = 0; j < morse.Length && !isLetterFound; j++)
+                        {
+                            for (int k = 0; k < morse[j].Length; k++)
+                            {
+                                if(morseTxt.Text.ToUpper()[i] == morse[j][k])
+                                {
+                                    newArray = morse[j].ToList();
+                                    newArray.Remove(morse[j][k]);
+                                    ShowMorse(newArray.ToArray());
+                                    isLetterFound = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private void ShowMorse(char[] morse)
+        {
+            string letter = null;
+            for (int i = 0; i < morse.Length; i++)
+            {
+               if(morse[i] == '.')
+                {
+                    letter += ".";
+                }
+                else
+                {
+                    letter += "-";
+                }
+            }
+            txtBlock.Text += letter;
+            txtBlock.Text += " ";
+        }
+        private void ShowMorse(char a)
+        {
+            txtBlock.Text += " ";
+            txtBlock.Text += "/";
+            txtBlock.Text += " ";
+
+        }
+
+        private void playBtn_Click(object sender, RoutedEventArgs e)
+        {
+            rectColor.Background = black;
+            
+            for (int i = 0; i < txtBlock.Text.Length; i++)
+            {
+
+                if (txtBlock.Text[i] == ' ')
+                    continue;
+
+                else if (txtBlock.Text[i] == '.')
+                {
+                    dotPlayer.PlaySync();
+                    rectColor.Background = white;
+                }
+                else
+                {
+                    dashPlayer.PlaySync();
+                    rectColor.Background = white;
+                    
+                }
+            }
+        }
+
+
+        private void txtBlock_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtBlock.Text))
+            {
+                playBtn.IsEnabled = false;
+                copyBtn.IsEnabled = false;
+            }
+            else
+            {
+                playBtn.IsEnabled = true;
+                copyBtn.IsEnabled = true;
+            }
+        }
+
+        private void copyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(txtBlock.Text);
+            MessageBox.Show("Morse code has been saved to clipboard!", "Saved successfully", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ MorseConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMorseCode
+{
+    class MorseConverter
+    {
+        public static char[][] MorseCode()
+        {
+            //Morse Code 
+            char[][] Morse = new char[36][];
+            //Alphabets
+            Morse[0] = new char[] { 'A', '.', '_' };
+            Morse[1] = new char[] { 'B', '_', '.', '.', '.' };
+            Morse[2] = new char[] { 'C', '_', '.', '_', '.' };
+            Morse[3] = new char[] { 'D', '_', '.', '.' };
+            Morse[4] = new char[] { 'E', '.' };
+            Morse[5] = new char[] { 'F', '.', '.', '_', '.' };
+            Morse[6] = new char[] { 'G', '_', '_', '.' };
+            Morse[7] = new char[] { 'H', '.', '.', '.', '.' };
+            Morse[8] = new char[] { 'I', '.', '.' };
+            Morse[9] = new char[] { 'J', '.', '_', '_', '_' };
+            Morse[10] = new char[] { 'K', '_', '.', '_' };
+            Morse[11] = new char[] { 'L', '.', '_', '.', '.' };
+            Morse[12] = new char[] { 'M', '_', '_' };
+            Morse[13] = new char[] { 'N', '_', '.' };
+            Morse[14] = new char[] { 'O', '_', '_', '_' };
+            Morse[15] = new char[] { 'P', '.', '_', '_', '.' };
+            Morse[16] = new char[] { 'Q', '_', '_', '.', '_' };
+            Morse[17] = new char[] { 'R', '.', '_', '.' };
+            Morse[18] = new char[] { 'S', '.', '.', '.' };
+            Morse[19] = new char[] { 'T', '_' };
+            Morse[20] = new char[] { 'U', '.', '.', '_' };
+            Morse[21] = new char[] { 'V', '.', '.', '.', '_' };
+            Morse[22] = new char[] { 'W', '.', '_', '_' };
+            Morse[23] = new char[] { 'X', '_', '.', '.', '_' };
+            Morse[24] = new char[] { 'Y', '_', '.', '_', '_' };
+            Morse[25] = new char[] { 'Z', '_', '_', '.', '.' };
+            //Numerics
+            Morse[27] = new char[] { '1', '.', '_', '_', '_', '_' };
+            Morse[28] = new char[] { '2', '.', '.', '_', '_', '_' };
+            Morse[29] = new char[] { '3', '.', '.', '.', '_', '_' };
+            Morse[30] = new char[] { '4', '.', '.', '.', '.', '_' };
+            Morse[31] = new char[] { '5', '.', '.', '.', '.', '.' };
+            Morse[31] = new char[] { '6', '_', '.', '.', '.', '.' };
+            Morse[32] = new char[] { '7', '_', '_', '.', '.', '.' };
+            Morse[33] = new char[] { '8', '_', '_', '_', '.', '.' };
+            Morse[34] = new char[] { '8', '_', '_', '_', '_', '.' };
+            Morse[35] = new char[] { '0', '_', '_', '_', '_', '_' };
+
+            return Morse;
+
+        }
+    }
+}
